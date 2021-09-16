7a48172381bcf70232c58ef8dbf279d2a38d8a84
mark-escosura <escosuramarkse@gmail.com>
2021-09-15 17:10:24 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -39,8 +39,9 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  // this.name = name;
+  // this. = false;
 }
 
 
@@ -63,9 +64,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+};
+
+Car.prototype.drive = function(distance) {
+  this.odometer += distance;
+  let ranOutOfFuel = distance / this.milesPerGallon;
+  this.tank -= ranOutOfFuel;
+};
+
+
+  
 
 
 /*
